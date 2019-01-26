0a028b7209e5f4c4c60d9c7d40ecdccfc5cfd4f3
avdata99 <andres@data99.com.ar>
2019-01-26 10:10:20 -0300

    merge 15

--- extras/mp3-to-video-with-waveform.sh
+++ extras/mp3-to-video-with-waveform.sh
@@ -1,8 +1,8 @@
 # crear video con la onda del audio de fondo y una imagen
 # https://stackoverflow.com/questions/34029575/showfreqs-and-showwaves-over-background-image
-TEXTO="Cadena de datos"
+TEXTO="CADENA DE DATOS"
 TTF_FILE="/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf"
-FONT_SIZE=10
+FONT_SIZE=30
 FONT_COLOR=black
 ANCHO=810
 ANCHO_WAVE1=300
@@ -11,7 +11,8 @@ ALTO_WAVE1=50
 ALTO_WAVE2=500
 ALTO_IMAGEN=618
 ALTO=ALTO_WAVE1 + ALTO_WAVE2 + ALTO_IMAGEN
-SAMPLESXCOLUMN=1
+SAMPLESXCOLUMN=3
+COLOR_ONDA=00AAAA
 # Modo de la onda:
 # point: Draw a point for each sample.
 # line: Draw a vertical line for each sample.
@@ -19,7 +20,7 @@ SAMPLESXCOLUMN=1
 # cline: Draw a centered vertical line for each sample.
 WAVE_MODE="cline"
 FILTER="[0:a]showfreqs=mode=bar:ascale=lin:fscale=lin:s=${ANCHO_WAVE1}x${ALTO_WAVE1}[sf]; \
-        [0:a]showwaves=s=${ANCHO_WAVE2}x${ALTO_WAVE2}:mode=${WAVE_MODE}:n=${SAMPLESXCOLUMN}:colors=red[sw]; \
+        [0:a]showwaves=s=${ANCHO_WAVE2}x${ALTO_WAVE2}:mode=${WAVE_MODE}:n=${SAMPLESXCOLUMN}:colors=${COLOR_ONDA}[sw]; \
         [sf][sw]vstack[fg]; \
         [1:v]scale=${ANCHO}:-1,crop=iw:${ALTO_IMAGEN}[bg]; \
         [bg][fg]overlay=shortest=1:format=auto,format=yuv420p,drawtext=fontfile=$TTF_FILE:fontsize=${FONT_SIZE}:fontcolor=${FONT_COLOR}:x=5:y=5:text='$TEXTO'[out]"
